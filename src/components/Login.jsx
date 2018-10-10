d22fd59899657122cdb27cbd24939db2863288b4
Supun Kannangara <supun.kannangara@pearson.com>
2018-10-10 17:48:09 +0530

    initial commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        Login
+        <input />
+        <button>Login</button>
+      </div>
+    );
+  }
+}
+
+export default Login;
